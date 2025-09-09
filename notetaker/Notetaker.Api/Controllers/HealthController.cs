feff14a7808104cf3879d4bcc0574cfb2f7125c3
Nayan VIthanage <nayan.vithanage@gmail.com>
2025-09-09 20:44:50 +0530

    Fix Railway deployment: use hardcoded values instead of environment variables in production config

--- notetaker/Notetaker.Api/Controllers/HealthController.cs
+++ notetaker/Notetaker.Api/Controllers/HealthController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Notetaker.Api.Data;
 
 namespace Notetaker.Api.Controllers;
 
@@ -8,37 +6,13 @@ namespace Notetaker.Api.Controllers;
 [Route("api/[controller]")]
 public class HealthController : ControllerBase
 {
-    private readonly NotetakerDbContext _context;
-
-    public HealthController(NotetakerDbContext context)
-    {
-        _context = context;
-    }
-
     [HttpGet]
-    public async Task<IActionResult> Get()
+    public IActionResult Get()
     {
-        try
-        {
-            // Test database connectivity
-            await _context.Database.CanConnectAsync();
-            
-            return Ok(new { 
-                status = "healthy", 
-                timestamp = DateTime.UtcNow,
-                version = "1.0.0",
-                database = "connected"
-            });
-        }
-        catch (Exception ex)
-        {
-            return StatusCode(503, new { 
-                status = "unhealthy", 
-                timestamp = DateTime.UtcNow,
-                version = "1.0.0",
-                database = "disconnected",
-                error = ex.Message
-            });
-        }
+        return Ok(new { 
+            status = "healthy", 
+            timestamp = DateTime.UtcNow,
+            version = "1.0.0"
+        });
     }
 }
